33612ea5ed4e7f285c3f9eefd2daa831045b5685
Timofey Balashov <tbalashov@gmail.com>
2014-10-22 17:56:35 +0600

    Initial commit

---
+++ src/main/java/com/epam/tb/hello/Runner.java
@@ -0,0 +1,12 @@
+package com.epam.tb.hello;
+
+import org.apache.log4j.Logger;
+
+public class Runner {
+
+    private static final Logger log = Logger.getLogger(Runner.class);
+
+    public static void main(String[] args) {
+        log.info("Hello World");
+    }
+}
